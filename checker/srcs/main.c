fe30efb0461a499f54d8dd4d3b421ef8ec85c56e
Aurelien Bucher <aurbuche@z2r11p4.42lyon.fr>
2021-04-10 16:33:28 +0200

    end of the week

--- checker/srcs/main.c
+++ checker/srcs/main.c
@@ -12,38 +12,72 @@
 
 #include "../../includes/checker.h"
 
-t_check *init_ck(t_check *ck, int ac)
+static void init_ck(t_check *ck, int ac)
 {
-	ck = malloc(sizeof(t_check *) + 1);
-	ck->tab = malloc(sizeof(int) * ac);
-	return ck;
+	ck->max = ac - 1;
+	ck->command_a = 0;
+	ck->command_b = 0;
+//	ck->max_a = ac - 1;
+//	ck->max_b = 0;
+	ck->stack_a = malloc(sizeof(int) * (ac - 1));
+	ck->stack_b = malloc(sizeof(int) * (ac - 1));
 }
 
-int	*fill_tab(int ac, char **av, t_check *ck)
+int	*fill_stack_a(int ac, char **av, t_check *ck)
 {
-	int		i;
+	int	i;
+	int j;
 
 	i = 0;
-	while (i <= ac)
+	j = 1;
+	while (i < ac - 1)
 	{
-		ck->tab[i] = ft_atoi(av[0]);
+		ck->stack_a[i] = ft_atoi(av[j]);
+		i++;
+		j++;
+	}
+	return ck->stack_a;
+}
+
+int check_arg(int ac, char **av)
+{
+	int	i;
+	int j;
+
+	i = 1;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (!ft_isdigit(av[i][j])
+				&& av[i][j] != ' ')
+				return (0);
+			j++;
+		}
 		i++;
 	}
-	ck->tab[i] = 0;
-	return ck->tab;
+	return (1);
 }
 
 int main(int ac, char **av)
 {
-	if (ac == 1)
-		return (0);
-	t_check *ck;
+	t_check ck[1];
 
-	ck = NULL;
-	ck = init_ck(ck, ac);
-	ck->tab = fill_tab(ac, av, ck);
-	for (int i = 0; i < ac; i++)
-		printf(" %d |", ck->tab[i]);
-	printf("\n");
+	if (!check_arg(ac, av))
+	{
+		printf("Error\n");
+		return (0);
+	}
+	init_ck(ck, ac);
+	ck->stack_a = fill_stack_a(ac, av, ck);
+	for (int i = 0; i < ac - 1; i++)
+		printf("%d  |  %d\n", i, ck->stack_a[i]);
+	free(ck->stack_a);
+	free(ck->stack_b);
+	while (1)
+	{
+		;
+	}
 	return 0;
 }
