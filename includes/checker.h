553ccc0443af0802dd86b568d5002cff1b8467d2
acharras <charras2311@gmail.com>
2021-04-13 13:40:31 +0200

    a jour sur master

--- includes/checker.h
+++ includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: acharras <acharras@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 12:31:09 by acharras          #+#    #+#             */
-/*   Updated: 2021/04/12 14:46:37 by acharras         ###   ########lyon.fr   */
+/*   Updated: 2021/04/12 16:33:30 by acharras         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,24 +41,24 @@ int			stack_command(t_ps *ps);
 long		ft_atoi_ps(const char *str);
 
 //OPTION FILE
-void	        ft_swap_a(t_ps *ps);
-void        	ft_swap_b(t_ps *ps);
-void			ft_swap_ss(t_ps *ps);
-void			ft_push_a(t_ps *ps);
-void			ft_push_b(t_ps *ps);
-void			ft_rotate_a(t_ps *ps);
-void			ft_rotate_b(t_ps *ps);
-void			ft_rotate_rr(t_ps *ps);
-void			ft_reverse_rotate_a(t_ps *ps);
-void			ft_reverse_rotate_b(t_ps *ps);
-void			ft_reverse_rotate_rr(t_ps *ps);
+int		        ft_swap_a(t_ps *ps);
+int	        	ft_swap_b(t_ps *ps);
+int				ft_swap_ss(t_ps *ps);
+int				ft_push_a(t_ps *ps);
+int				ft_push_b(t_ps *ps);
+int				ft_rotate_a(t_ps *ps);
+int				ft_rotate_b(t_ps *ps);
+int				ft_rotate_rr(t_ps *ps);
+int				ft_reverse_rotate_a(t_ps *ps);
+int				ft_reverse_rotate_b(t_ps *ps);
+int				ft_reverse_rotate_rr(t_ps *ps);
 
 void	        ft_sort_push(t_ps *sort, char **push);
 void	        ft_command(t_ps *sort, char **argv);
 int				ft_is_sorted(t_ps *sort, int argc);
 int				ft_max_stack(int *stack);
-int				*ft_up_stack(int *stack);
-int				*ft_down_stack(int *stack);
+int				*ft_up_stack(int *stack, int len);
+int				*ft_down_stack(int *stack, int len);
 
 
 
